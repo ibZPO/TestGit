270f0e1aa27db9b8d7f60a0ab2177a8bfda0e559
Michal Adamski <michal.adamski@pwr.edu.pl>
2023-04-18 20:01:44 +0200

    feat: Poin distance calculation method added

--- src/main/java/edu/ib/PointTester.java
+++ src/main/java/edu/ib/PointTester.java
@@ -5,8 +5,10 @@ public class PointTester {
     public static void main(String[] args) {
 
         Point p1= new Point(10,10);
+        Point p2= new Point(0,10);
 
         System.out.println(p1);
+        System.out.println(p1.calculateDistance(p2));
 
         System.out.println("Hello world");
     }

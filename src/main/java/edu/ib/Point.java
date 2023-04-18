ea80130a4e8d6d0a4c1f52815944e767b7780852
ibZPO <75097673+ibZPO@users.noreply.github.com>
2023-04-18 19:26:54 +0200

    Initial commit

---
+++ src/main/java/edu/ib/Point.java
@@ -0,0 +1,13 @@
+package edu.ib;
+
+public class Point {
+
+    private  float x;
+    private float y;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
---
+++ src/main/java/edu/ib/PointTester.java
@@ -0,0 +1,9 @@
+package edu.ib;
+
+public class PointTester {
+
+    public static void main(String[] args) {
+
+        Point p1= new Point(10,10);
+    }
+}
